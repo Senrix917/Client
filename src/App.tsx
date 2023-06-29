511755faa01e53f127da866c444a0de33075034e
justin <just.frauboese@gmial.com>
2023-06-29 22:57:25 +0200

    add Client

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import CharacterData from "./webScraper";
+import { Button, Space, Input, Typography } from "antd";
 
-function App() {
+function App(): JSX.Element {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [showCharacterData, setShowCharacterData] = useState<boolean>(false);
+  const [parameter, setParameter] = useState<string>("");
+  
+
+  const {Title} = Typography
+
+  function refreshPage() {
+    window.location.reload();
+  }
+
+  const handleClick = (): void => {
+    // Pass the inputValue to the CharacterData component or perform any other action
+    setShowCharacterData(true);
+    setParameter(inputValue);
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (event.key === "Enter") {
+      if (inputValue.length <= 20) {
+        setShowCharacterData(true);
+        setParameter(inputValue)
+      }
+  }};
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.value.length <= 20) {
+      setInputValue(event.target.value);
+    }}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <Title>Framedata</Title>
+      <Space.Compact style={{ width: '100%' }}>
+      <Input defaultValue="Character Name" value={inputValue} onChange={handleChange} onKeyPress={handleKeyPress}/>
+      <Button type="primary" onClick={handleClick}>Submit</Button>
+    </Space.Compact>
+
+      {showCharacterData && <CharacterData parameter={parameter} />}
+
+      <Button type="primary" onClick={refreshPage}>
+        Click to reload!
+      </Button>
     </div>
   );
 }
